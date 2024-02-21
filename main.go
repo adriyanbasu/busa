813f52d856f463b1e4a675c424efc149ded8201b
Prasenjit Basu <144541917+adriyanbasu@users.noreply.github.com>
2024-02-21 00:03:17 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"unicode"
+)
+
+type Token struct {
+	value  string
+	kind   TokenKind
+	length int
+}
+
+type TokenKind int
+
+const (
+	Unknown TokenKind = iota
+	Number
+	Operator
+	Identifier
+	String
+	Whitespace
+	EOF
+	Error
+	Comment
+	Keyword
+	Symbol
+	Punctuator
+	UnknownPunctuator
+	KeywordPunctuator
+	SymbolPunctuator
+	WhitespacePunctuator
+)
+
+func tokenize(input string) []Token {
+	tokens := lex([]rune(input))
+	tokens = append(tokens, Token{value: "EOF", kind: EOF, length: 0})
+	return tokens
+}
+
+func lexToken(input []rune) *Token {
+	if len(input) == 0 {
+		return nil
+	}
+	if unicode.IsSpace(input[0]) {
+		return &Token{
+			value:  " ",
+			length: 1,
+		}
+	}
+	return nil
+}
+
+func lex(input []rune) (tokens []Token) {
+	curser := 0
+	for len(input) > 0 {
+		token := lexToken(input)
+		if token != nil {
+			tokens = append(tokens, *token)
+			input = input[token.length:]
+			curser += token.length
+			continue
+		}
+		if len(input) == 0 {
+			break
+		}
+		tokens = append(tokens, Token{
+			value:  string(input[0]),
+			length: 1,
+		})
+		input = input[token.length:]
+		curser += token.length
+	}
+	return tokens
+}
